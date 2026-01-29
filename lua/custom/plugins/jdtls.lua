3bc2d709187f722d80b31253677c4f041aee5e15
Jiří Trefil <jirka@Jiris-MacBook-Pro.local>
2026-01-29 12:19:27 +0100

    commented out the attach so jdtls does not start everytime

--- lua/custom/plugins/jdtls.lua
+++ lua/custom/plugins/jdtls.lua
@@ -80,7 +80,7 @@ return {
       }
       -- This starts a new client & server,
       -- or attaches to an existing client & server depending on the `root_dir`.
-      require('jdtls').start_or_attach(config)
+      --require('jdtls').start_or_attach(config)
     end,
   },
 }
