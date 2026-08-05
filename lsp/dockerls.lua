7d7ca2814449c1090b20b0e0f5ac62ab86fc9294
Jiří Trefil <jirka@Jiris-MacBook-Pro.local>
2026-08-05 12:07:45 +0200

    docker and terraform lsps

---
+++ lsp/dockerls.lua
@@ -0,0 +1,7 @@
+-- npm install -g dockerfile-language-server-nodejs
+---@type vim.lsp.Config
+return {
+  cmd = { "docker-langserver", "--stdio" },
+  filetypes = { "dockerfile" },
+  root_markers = { ".git" },
+}
---
+++ lsp/terraformls.lua
@@ -0,0 +1,7 @@
+-- brew install terraform-ls
+---@type vim.lsp.Config
+return {
+  cmd = { "terraform-ls", "serve" },
+  filetypes = { "terraform", "terraform-vars" },
+  root_markers = { ".terraform", ".git" },
+}
